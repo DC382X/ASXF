5250fb980fd836c88c00ed0d08ac7bb60be5bbab
DC382X <82519449+DC382X@users.noreply.github.com>
2021-05-17 07:25:31 -0500

    Create Checkbox Value.js

---
+++ Spare Resources/Checkbox Value.js
@@ -0,0 +1,3 @@
+document.getElementById("ispum").checked = true; // Set Value to true
+document.getElementById("dolor").checked = false; // Set Value to false
+var lorem = document.getElementById("sit").checked // Get the value
